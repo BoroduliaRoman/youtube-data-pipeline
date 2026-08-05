49c1b6d846d0076d4a80832b5fd0375c6774416d
romeo <boroduliaroman@gmail.com>
2026-08-05 23:54:47 +0300

    Add script: Lodaing the raw JSON data

---
+++ dags/datawarehouse/data_loading.py
@@ -0,0 +1,29 @@
+import json
+from datetime import date
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def load_path() -> list[dict]:
+    """Load today's YouTube API data from a JSON file."""
+    
+    today = date.today()
+
+    file_path = f'./data/youtube_data_{today}.json'
+
+    try:
+        logger.info(f"Processing file: youtube_data_{today}.json")
+
+        with open(file_path, 'r', encoding='utf-8') as raw_data:
+            data = json.load(raw_data)
+
+        return data
+
+    except FileNotFoundError:
+        logger.error(f"File not found: {file_path}")
+        raise
+    except json.JSONDecodeError:
+        logger.error(f"Invalid JSON in file: {file_path}")
+        raise
